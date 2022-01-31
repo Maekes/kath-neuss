253ac247cc560b1ea1f4787f46fff0db92b8fea8
Markus Marcinek <markus.marcinek@outlook.de>
2022-01-31 12:37:42 +0100

    verstecke ungenutzte symbole

--- template-parts/section-person.php
+++ template-parts/section-person.php
@@ -25,20 +25,24 @@
             </p>
         <?php endif; ?>
         <div class="flex justify-center space-x-2">
-            <a class="hover:text-red-700 text-black" href="mailto:#">
+            <?php if (get_field('mailadresse')) : ?>
+            <a class="hover:text-red-700 text-black" href="mailto:<?php the_field('mailadresse'); ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current w-8 h-8 stroke-1" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <rect x="3" y="5" width="18" height="14" rx="2" />
                     <polyline points="3 7 12 13 21 7" />
                 </svg>
             </a>
-            <a class="hover:text-red-700 text-black" href="mailto:#">
+            <?php endif; ?>
+            <?php if (get_field('instagram')) : ?>
+            <a class="hover:text-red-700 text-black" href="<?php the_field('instagram'); ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current w-8 h-8 stroke-1" viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <rect x="4" y="4" width="16" height="16" rx="4" />
                     <circle cx="12" cy="12" r="3" />
                     <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
             </a>
+            <?php endif; ?>
         </div>
     </div>
 </div>
