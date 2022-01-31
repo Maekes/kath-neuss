7679923504a2a720f5ac44423b1e5b8bd79df827
Markus Marcinek <markus.marcinek@outlook.de>
2022-01-31 09:49:11 +0100

    remove svg

--- functions.php
+++ functions.php
@@ -188,35 +188,6 @@ add_filter('excerpt_length', 'mytheme_custom_excerpt_length', 999);
 add_filter('wpcf7_autop_or_not', '__return_false');
 
 
-/**
- * Add svg support
- **/
-add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
-	global $wp_version;
-	if ($wp_version == '4.7' || ((float) $wp_version < 4.7)) {
-		return $data;
-	}
-	$filetype = wp_check_filetype($filename, $mimes);
-	return [
-		'ext'             => $filetype['ext'],
-		'type'            => $filetype['type'],
-		'proper_filename' => $data['proper_filename']
-	];
-}, 10, 4);
-
-function dl_mime_types($mimes)
-{
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
-}
-add_filter('upload_mimes', 'dl_mime_types');
-
-function dl_fix_svg()
-{
-	echo '<style type="text/css">.attachment-266x266, .thumbnail img { width: 100% !important; height: auto !important;} </style>';
-}
-add_action('admin_head', 'dl_fix_svg');
-
 
 /* ACF BLOCKS */
 
