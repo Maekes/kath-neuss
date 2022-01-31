b59046b3c11398775fe5d658ec6314b484e175e6
Markus Marcinek <markus.marcinek@outlook.de>
2022-01-31 15:49:00 +0100

    fix

--- archive-gruppen.php
+++ archive-gruppen.php
@@ -5,7 +5,35 @@
     <h1><?php post_type_archive_title() ?></h1>
 
 
-       
+    <?php 
+        // Source: https://code.tutsplus.com/tutorials/taxonomy-archives-list-posts-by-taxonomys-terms--cms-20045
+        //start by fetching the terms for gruppen taxonomy
+        $terms = get_terms( 'gruppenkategorie', array(
+            'orderby'    => 'count',
+            'hide_empty' => 0
+        ) );
+
+        foreach( $terms as $term ) {
+            echo'<h2>' . $term->name . '</h2>';
+            // Define the query
+            $args = array(
+                'post_type' => 'gruppen',
+                'gruppen' => $term->slug
+            );
+            $query = new WP_Query( $args );
+   
+            
+            // Start the Loop
+            while ( $query->have_posts() ) : $query->the_post(); 
+
+                get_template_part('template-parts/section', 'archive-default'); 
+            
+            endwhile;
+            
+            // use reset postdata to restore orginal query
+            wp_reset_postdata();
+        } 
+        ?>
 
 
 </div>
