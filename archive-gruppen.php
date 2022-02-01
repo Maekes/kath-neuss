a98cdd9095ee4db713bd855e192cf1d5c79115c6
Markus Marcinek <markus.marcinek@outlook.de>
2022-02-01 11:03:31 +0100

    add im aufbau

--- archive-gruppen.php
+++ archive-gruppen.php
@@ -3,38 +3,38 @@
 <div class="mx-auto mb-8">
 
     <h1><?php post_type_archive_title() ?></h1>
+    <i>Im Aufbau....</i>
+
+    <?php
+    // Source: https://code.tutsplus.com/tutorials/taxonomy-archives-list-posts-by-taxonomys-terms--cms-20045
+    //start by fetching the terms for gruppen taxonomy
+    $terms = get_terms('gruppenkategorie', array(
+        'orderby'    => 'count',
+        'hide_empty' => 0,
+        'partent' => 0
+    ));
+
+    foreach ($terms as $term) {
+        echo '<h2>' . $term->name . '</h2>';
+        // Define the query
+        $args = array(
+            'post_type' => 'gruppen',
+            'gruppenkategorie' => $term->slug
+        );
+        $query = new WP_Query($args);
 
 
-    <?php 
-        // Source: https://code.tutsplus.com/tutorials/taxonomy-archives-list-posts-by-taxonomys-terms--cms-20045
-        //start by fetching the terms for gruppen taxonomy
-        $terms = get_terms( 'gruppenkategorie', array(
-            'orderby'    => 'count',
-            'hide_empty' => 0,
-            'partent' => 0
-        ) );
+        // Start the Loop
+        while ($query->have_posts()) : $query->the_post();
 
-        foreach( $terms as $term ) {
-            echo'<h2>' . $term->name . '</h2>';
-            // Define the query
-            $args = array(
-                'post_type' => 'gruppen',
-                'gruppenkategorie' => $term->slug
-            );
-            $query = new WP_Query( $args );
-   
-            
-            // Start the Loop
-            while ( $query->have_posts() ) : $query->the_post(); 
+            get_template_part('template-parts/section', 'archive-default');
 
-                get_template_part('template-parts/section', 'archive-default'); 
-            
-            endwhile;
-            
-            // use reset postdata to restore orginal query
-            wp_reset_postdata();
-        } 
-        ?>
+        endwhile;
+
+        // use reset postdata to restore orginal query
+        wp_reset_postdata();
+    }
+    ?>
 
 
 </div>
