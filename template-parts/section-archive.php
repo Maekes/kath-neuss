9d3df59560bf3888435804b6d3782b22797c0a31
Markus Marcinek <markus.marcinek@outlook.de>
2022-02-14 20:58:38 +0100

    change archive image size

--- template-parts/section-archive.php
+++ template-parts/section-archive.php
@@ -1,5 +1,5 @@
 <a id="post-<?php the_ID(); ?>" class="w-full bg-white rounded-lg shadow overflow-hidden  flex items-top flex-row " href="<?php the_permalink(); ?>">
-    <div class="flex-shrink-0 h-auto self-stretch w-24 sm:w-40 md:w-56 bg-cover bg-center border-r border-gray-100" style="background-image:url(<?php the_post_thumbnail_url("small"); ?>)">
+    <div class="flex-shrink-0 h-auto self-stretch w-24 sm:w-40 md:w-56 bg-cover bg-center border-r border-gray-100" style="background-image:url(<?php the_post_thumbnail_url("medium"); ?>)">
     </div>
     <div class="px-5 py-2">
         <span class="block text-xl leading-tight font-semibold text-black "><?php the_title(); ?></span>
