3bb41e05c8ba2e82c61a5a67a350b4737433a3bb
Markus Marcinek <markus.marcinek@outlook.de>
2022-02-14 19:59:21 +0100

    test

--- template-parts/section-person-in-sidebar.php
+++ template-parts/section-person-in-sidebar.php
@@ -1,6 +1,6 @@
 <div class="flex items-top space-x-4 md:max-w-md">
 
-    <img class="object-cover rounded-lg overflow-hidden flex-none h-40 w-28 shadow-md" src="<?php the_post_thumbnail_url("small"); ?>">
+    <img class="object-cover rounded-lg overflow-hidden flex-none h-40 w-28 shadow-md" src="<?php the_post_thumbnail_url("thumbnail"); ?>">
 
     <div class="mx-auto text-sm text-left">
         <div class="mb-2">
